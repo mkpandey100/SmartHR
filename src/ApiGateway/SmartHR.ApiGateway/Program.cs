17a89985bef2a9c7430a15fc16d0075617946874
Mahendra Pandey <mpandey@devfinity.com>
2025-04-11 09:12:00 +0545

    - fixed port issue

--- src/ApiGateway/SmartHR.ApiGateway/Program.cs
+++ src/ApiGateway/SmartHR.ApiGateway/Program.cs
@@ -3,48 +3,37 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load Ocelot routes
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Configuration/ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddControllers();
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Load Ocelot Routes from JSON
-builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("Configuration/ocelot.json", optional: false, reloadOnChange: true);
-
-// Dynamically load other route configuration files (except ocelot.json)
-//foreach (var file in Directory.GetFiles("Configuration", "*.json"))
-//{
-//    if (!file.EndsWith("ocelot.json"))
-//    {
-//        builder.Configuration.AddJsonFile(file, optional: false, reloadOnChange: true);
-//    }
-//}
-
 builder.Services.AddOcelot(builder.Configuration);
-
-builder.Logging.AddConsole();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartHR Gateway V1");
+    c.RoutePrefix = "";
+});
 
 app.UseRouting();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
-app.UseCors("AllowAll");
-app.UseOcelot().Wait();
 
-app.UseOcelot().ConfigureAwait(false).GetAwaiter().OnCompleted(() =>
-{
-    Console.WriteLine("Request handled by Ocelot");
-});
+await app.UseOcelot();
 
 app.Run();
